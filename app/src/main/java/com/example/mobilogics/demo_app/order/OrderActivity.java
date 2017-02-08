dad9d3252cecd18d04d448729519388da661be10
johnson129515 <johnson129515@gmail.com>
2017-02-09 01:22:50 +0800

    feat : the order app first screen layout complete

--- app/src/main/java/com/example/mobilogics/demo_app/order/OrderActivity.java
+++ app/src/main/java/com/example/mobilogics/demo_app/order/OrderActivity.java
@@ -2,6 +2,8 @@ package com.example.mobilogics.demo_app.order;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.example.mobilogics.demo_app.R;
 
@@ -11,10 +13,44 @@ import com.example.mobilogics.demo_app.R;
 
 public class OrderActivity extends AppCompatActivity{
 
+    private Spinner mCoffeeSpinner;
+
+    private Spinner mQuantity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
         setTitle(getString(R.string.function_1));
+
+        mCoffeeSpinner = (Spinner)findViewById(R.id.coffee_type_spinner);
+        mQuantity = (Spinner)findViewById(R.id.coffee_quantity);
+
+        setCoffeeSpinner();
+        setQuantitySpinner();
+    }
+
+    /**
+     * Setup the coffee types spinner
+     */
+
+    private void setCoffeeSpinner(){
+
+        ArrayAdapter coffeeSpinnerAdapter = ArrayAdapter.createFromResource(
+                this,R.array.coffee_type , android.R.layout.simple_spinner_item);
+
+        coffeeSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
+
+        mCoffeeSpinner.setAdapter(coffeeSpinnerAdapter);
+    }
+
+    private void setQuantitySpinner(){
+
+        ArrayAdapter quantitySpinnerAdapter = ArrayAdapter.createFromResource(
+                this,R.array.quantity , android.R.layout.simple_spinner_item);
+
+        quantitySpinnerAdapter.setDropDownViewResource(R.layout.quantity_spinner_style);
+
+        mQuantity.setAdapter(quantitySpinnerAdapter);
     }
 }
