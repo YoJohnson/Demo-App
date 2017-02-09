b1403dbd9dcd6e370753850529b583a8c157a874
johnson129515 <johnson129515@gmail.com>
2017-02-09 14:27:20 +0800

    feat : complete the coffee image show

--- app/src/main/java/com/example/mobilogics/demo_app/order/OrderActivity.java
+++ app/src/main/java/com/example/mobilogics/demo_app/order/OrderActivity.java
@@ -3,8 +3,13 @@ package com.example.mobilogics.demo_app.order;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.Spinner;
+import android.widget.Switch;
+import android.widget.Toast;
 
 import com.example.mobilogics.demo_app.R;
 
@@ -18,6 +23,8 @@ public class OrderActivity extends AppCompatActivity{
 
     private Spinner mQuantity;
 
+    private ImageView mCoffeeImage;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,6 +33,7 @@ public class OrderActivity extends AppCompatActivity{
 
         mCoffeeSpinner = (Spinner)findViewById(R.id.coffee_type_spinner);
         mQuantity = (Spinner)findViewById(R.id.coffee_quantity);
+        mCoffeeImage = (ImageView) findViewById(R.id.coffee_image);
 
         setCoffeeSpinner();
         setQuantitySpinner();
@@ -43,6 +51,47 @@ public class OrderActivity extends AppCompatActivity{
         coffeeSpinnerAdapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
 
         mCoffeeSpinner.setAdapter(coffeeSpinnerAdapter);
+
+        //set the item select listener to change the image
+        mCoffeeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+                switch (position){
+
+                    case 0:
+                        Toast.makeText(OrderActivity.this, getString(R.string.choose_coffee),Toast.LENGTH_SHORT).show();
+
+                        return;
+
+                    case 1:
+                        mCoffeeImage.setImageResource(R.drawable.espresso);
+
+                        return;
+
+                    case 2:
+                        mCoffeeImage.setImageResource(R.drawable.cappuccino);
+
+                        return;
+
+                    case 3:
+                        mCoffeeImage.setImageResource(R.drawable.mocha);
+
+                        return;
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                Toast.makeText(OrderActivity.this, getString(R.string.choose_coffee),Toast.LENGTH_SHORT).show();
+            }
+        });{
+
+        };
+
+
+
     }
 
     private void setQuantitySpinner(){
